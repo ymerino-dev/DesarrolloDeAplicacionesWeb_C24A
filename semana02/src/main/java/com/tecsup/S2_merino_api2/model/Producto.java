efddc8394f521c3eef7a59af1a4be25026fc5fc1
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-05 20:11:17 -0500

    Completado Laboratorio 2: Sistema Web de Productos con MVC y BD

---
+++ semana02/src/main/java/com/tecsup/S2_merino_api2/model/Producto.java
@@ -0,0 +1,46 @@
+package com.tecsup.S2_merino_api2.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "producto")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private double precio;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
---
+++ semana02/src/main/java/com/tecsup/S2_merino_api2/service/ProductoService.java
@@ -0,0 +1,18 @@
+package com.tecsup.S2_merino_api2.service;
+
+import com.tecsup.S2_merino_api2.model.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> listarProductos() {
+        return productoRepository.findAll();
+    }
+}
